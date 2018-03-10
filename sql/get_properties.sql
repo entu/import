36efb444678515f2c790b4cd6a58403844526556
Argo Roots <argo.roots@lhv.ee>
2018-03-10 16:08:35 +0200

    Property order changed

--- sql/get_properties.sql
+++ sql/get_properties.sql
@@ -11,15 +11,15 @@ SELECT
     value_reference AS `reference`,
     value_date AS `date`,
     CASE type
-        WHEN '_created' THEN created_at
-        WHEN '_deleted' THEN deleted_at
-        ELSE NULL
-    END AS `at`,
-    CASE type
         WHEN '_created' THEN created_by
         WHEN '_deleted' THEN deleted_by
         ELSE NULL
     END AS `by`,
+    CASE type
+        WHEN '_created' THEN created_at
+        WHEN '_deleted' THEN deleted_at
+        ELSE NULL
+    END AS `at`,
     created_at,
     created_by,
     deleted_at,
