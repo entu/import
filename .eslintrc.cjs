42bf1790b32616b33e9cb11992e5a9615de016e5
Argo Roots <argo@roots.ee>
2022-06-08 20:09:49 +0300

    Fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true
   },
   extends: [
     'standard'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
   }
