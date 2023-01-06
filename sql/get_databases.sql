16039a0bc0da3fe2060e9132aa31731d9c577aaf
Argo Roots <argo@roots.ee>
2023-01-06 13:07:54 +0200

    Ignore _template db

--- sql/get_databases.sql
+++ sql/get_databases.sql
@@ -1,6 +1,8 @@
 SELECT DISTINCT TABLE_SCHEMA AS db
 FROM information_schema.TABLES
 WHERE TABLE_SCHEMA NOT IN (
+    '_template',
+    '_template_minimal',
     'information_schema',
     'mysql',
     'performance_schema',
