a379e93b558f8cc75384aa83cdeee4b9df0fbc21
Argo Roots <argo@roots.ee>
2024-04-09 14:44:46 +0300

    Fix filename encoding

--- import/entities.js
+++ import/entities.js
@@ -348,7 +348,7 @@ async function copyFiles (database) {
       const putCommand = new PutObjectCommand({
         Bucket: process.env.DO_BUCKET,
         Key: `${database}/${entity.toString().substring(0, 4)}/${entity.toString().substring(4)}/${_id}`,
-        ContentDisposition: `inline; filename*=UTF-8''${filename}`,
+        ContentDisposition: `inline;filename="${encodeURI(filename.replace('"', '\"'))}"`,
         ContentType: mime.lookup(filename) || 'application/octet-stream',
         Body: fileBuffer
       })
